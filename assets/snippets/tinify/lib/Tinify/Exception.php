4524a94bd9811823d2311d51cb7724ef689bfd4c
ywx548750 <yurchenko.vladislav@huawei.com>
2020-10-24 16:31:41 +0500

    add getMessage

--- assets/snippets/tinify/lib/Tinify/Exception.php
+++ assets/snippets/tinify/lib/Tinify/Exception.php
@@ -4,11 +4,6 @@ namespace Tinify;
 
 class Exception extends \Exception {
     public $status;
-//     public $message;
-//
-//     public static function getMessage() {
-//         return $this->message;
-//     }
 
     public static function create($message, $type, $status) {
         if ($status == 401 || $status == 429) {
@@ -27,7 +22,7 @@ class Exception extends \Exception {
 
     function __construct($message, $type = NULL, $status = NULL) {
         $this->status = $status;
-//         $this->message = $message;
+        
         if ($status) {
             parent::__construct($message . " (HTTP " . $status . "/" . $type . ")");
         } else {
